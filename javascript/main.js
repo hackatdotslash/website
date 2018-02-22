bf8920967aeb8e04f47d9e0e9b0ae196cd3b71a6
Unknown <avinashjaiswal642@gmail.com>
2018-02-23 02:15:25 +0530

    Bux fixes: Enable Responsiveness

--- javascript/main.js
+++ javascript/main.js
@@ -7,3 +7,59 @@ function myFunction() {
         document.querySelector(".navbar").classList.remove("scrolled");
     }
 }
+var currentDate = new Date().getTime() / 1000;
+        var futureDate = new Date("April 17, 2018 10:00:00").getTime() / 1000;
+
+        $(document).ready(function () {
+          $('#countdown9').ClassyCountdown({
+
+
+            end: futureDate,
+            now: currentDate,
+            labels: true,
+            style: {
+              element: "",
+              textResponsive: .5,
+              days: {
+                gauge: {
+                  thickness: .10,
+                  bgColor: "rgba(0,0,0,0)",
+                  fgColor: " #a8c0ff",
+                  lineCap: 'round'
+                },
+                textCSS: 'font-weight:300; color:#3f2b96;'
+              },
+              hours: {
+                gauge: {
+                  thickness: .10,
+                  bgColor: "rgba(0,0,0,0)",
+                  fgColor: " #a8c0ff",
+                  lineCap: 'round'
+                },
+                textCSS: ' font-weight:300; color:#3f2b96;'
+              },
+              minutes: {
+                gauge: {
+                  thickness: .10,
+                  bgColor: "rgba(0,0,0,0)",
+                  fgColor: " #a8c0ff",
+                  lineCap: 'round'
+                },
+                textCSS: ' font-weight:300; color:#3f2b96;'
+              },
+              seconds: {
+                gauge: {
+                  thickness: .10,
+                  bgColor: "rgba(0,0,0,0)",
+                  fgColor: " #a8c0ff",
+                  lineCap: 'round'
+                },
+                textCSS: ' font-weight:300; color:#3f2b96;'
+              }
+
+            },
+            onEndCallback: function () {
+              console.log("Time out!");
+            }
+          });
+        });

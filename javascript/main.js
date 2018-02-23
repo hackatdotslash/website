b6c0091708e0484c0fac2933f6b545be9aa29514
Unknown <avinashjaiswal642@gmail.com>
2018-02-23 12:46:00 +0530

    Recent dark theme

--- javascript/main.js
+++ javascript/main.js
@@ -7,13 +7,29 @@ function myFunction() {
         document.querySelector(".navbar").classList.remove("scrolled");
     }
 }
+
+var scene1 = document.getElementById('scene1');
+var parallaxInstance1 = new Parallax(scene1,{
+});
+var rotateTween= new TimelineMax({repeat:-1}).to(".circle",3,{rotation:360});
+var hack = new CircleType(document.getElementById('hack_text')).dir(-1).radius(450);
 var currentDate = new Date().getTime() / 1000;
         var futureDate = new Date("April 17, 2018 10:00:00").getTime() / 1000;
 
         $(document).ready(function () {
+            $('nav a').click(function(){
+                $('html, body').animate({
+                    scrollTop: $( $.attr(this, 'href') ).offset().top-50
+                }, 500);
+                return true;
+            });
+            $('body').scrollspy({ target: '#navbar-example' });
+            $('.collapse').collapse({
+                toggle: false,
+              });
           $('#countdown9').ClassyCountdown({
 
-
+            
             end: futureDate,
             now: currentDate,
             labels: true,
@@ -22,36 +38,36 @@ var currentDate = new Date().getTime() / 1000;
               textResponsive: .5,
               days: {
                 gauge: {
-                  thickness: .10,
+                  thickness: .20,
                   bgColor: "rgba(0,0,0,0)",
-                  fgColor: " #a8c0ff",
+                  fgColor: " #037BB4",
                   lineCap: 'round'
                 },
                 textCSS: 'font-weight:300; color:#3f2b96;'
               },
               hours: {
                 gauge: {
-                  thickness: .10,
+                  thickness: .20,
                   bgColor: "rgba(0,0,0,0)",
-                  fgColor: " #a8c0ff",
+                  fgColor: " #037BB4",
                   lineCap: 'round'
                 },
                 textCSS: ' font-weight:300; color:#3f2b96;'
               },
               minutes: {
                 gauge: {
-                  thickness: .10,
+                  thickness: .20,
                   bgColor: "rgba(0,0,0,0)",
-                  fgColor: " #a8c0ff",
+                  fgColor: " #037BB4",
                   lineCap: 'round'
                 },
                 textCSS: ' font-weight:300; color:#3f2b96;'
               },
               seconds: {
                 gauge: {
-                  thickness: .10,
+                  thickness: .20,
                   bgColor: "rgba(0,0,0,0)",
-                  fgColor: " #a8c0ff",
+                  fgColor: "#037BB4",
                   lineCap: 'round'
                 },
                 textCSS: ' font-weight:300; color:#3f2b96;'

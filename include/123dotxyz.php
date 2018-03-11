fd11122a5f8c31ec503e1f7fca0950b888d19933
Deepanshu sharma <U15CO061@coed.svnit.ac.in>
2018-03-11 12:43:14 +0530

    Minor Update

--- include/123dotxyz.php
+++ include/123dotxyz.php
@@ -1,9 +1,13 @@
 <?php
 require_once("config.php");
         $db_handle = new DBController();
+        //echo "string";
         $query = "SELECT * FROM registrationNew;";
         $query1 = "SELECT * FROM registration;";
         $total_team = $db_handle->numRows($query);
         $total_team1 = $db_handle->numRows($query1);
-        echo ""+$total_team1+" "+$total_team;
+        echo "First: ";
+        echo ""+$total_team1;
+        echo "\nSecond: ";
+        echo ""+$total_team;
 ?>
